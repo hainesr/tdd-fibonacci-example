1f1e54845346a110514198979e9ee1d8544fe50f
Robert Haines <rhaines@manchester.ac.uk>
2014-02-10 18:17:47 +0000

    Step 5: fibonacci(4).
    Another test and another extension to our naive implementation.

--- src/fibonacci.py
+++ src/fibonacci.py
@@ -9,4 +9,5 @@
 def fibonacci(n):
     if n == 0: return 0
     if n <= 2: return 1
-    return 2
+    if n == 3: return 2
+    return 3
--- test/test_fibonacci.py
+++ test/test_fibonacci.py
@@ -16,6 +16,7 @@ class TestFibonacci(unittest.TestCase):
         self.assertEqual(1, fibonacci(1), "fibonacci(1) should equal 1")
         self.assertEqual(1, fibonacci(2), "fibonacci(2) should equal 1")
         self.assertEqual(2, fibonacci(3), "fibonacci(3) should equal 2")
+        self.assertEqual(3, fibonacci(4), "fibonacci(4) should equal 3")
 
 if __name__ == "__main__":
     unittest.main()
